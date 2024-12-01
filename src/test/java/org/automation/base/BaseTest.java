0389702d9e2bed76265945fe28259735eb918466
darkmist-98 <garnaik77@gmail.com>
2024-12-01 17:40:03 +0530

    second commit - fixed BaseTest file

--- src/test/java/org/automation/base/BaseTest.java
+++ src/test/java/org/automation/base/BaseTest.java
@@ -1,13 +1,10 @@
 package org.automation.base;
 
-import org.automation.listener.EventListener;
 import org.automation.utils.ConfigReader;
 import org.automation.utils.DriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -18,10 +15,9 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    @Parameters({"browser","headless"})
-    public void init(String browser, String headless) {
-        System.setProperty("browser",browser);
-        System.setProperty("headless",headless);
+    public void init() {
+//        System.setProperty("browser",browser);
+//        System.setProperty("headless",headless);
         DriverManager.setDriver();
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         getDriver().manage().deleteAllCookies();
